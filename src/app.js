93f1aaf8d95ea594fbdee8664a6037bfcc436813
Paulo henrique <paulobatista.sistemas@gmail.com>
2021-03-09 20:56:06 -0300

    Desafio completo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,96 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const {title, url, techs} = request.body;
+
+  const repositorie = {
+    'title': title, 
+    'url': url,
+    'techs' : techs,
+    'id': uuid(),
+    'likes': 0
+  }
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const {title, url, techs} = request.body;
+  const {id} = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({ error: 'O parametro ID não é valido'});
+  }
+
+  const repositorieIndex = repositories.findIndex(r => r.id === id);
+  const repositorie = repositories.find((r) => r.id === id );
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({error: 'Respositório não encontrado.'});
+  }
+
+  const newRepositorie = {
+    id, 
+    title, 
+    url, 
+    techs, 
+    'likes': repositorie.likes
+  }
+
+  repositories[repositorieIndex] = newRepositorie;
+
+  return response.json(newRepositorie);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+
+    const {id} = request.params;
+
+    if(!isUuid(id)){
+      return response.status(400).json({ error: 'O parametro ID não é valido'});
+    }
+
+    const repositorieIndex = repositories.findIndex(r => r.id === id);
+
+    if(repositorieIndex < 0){
+      return response.status(400).json({error: 'Respositório não encontrado.'});
+    }
+
+    repositories.splice(repositorieIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+    const {id} = request.params;
+
+    if(!isUuid(id)){
+      return response.status(400).json({ error: 'O parametro ID não é valido'});
+    }
+
+    const repositorieIndex = repositories.findIndex(r => r.id === id);
+
+    if(repositorieIndex < 0){
+      return response.status(400).json({error: 'Respositório não encontrado.'});
+    }
+
+    repositories[repositorieIndex].likes += 1;
+    
+    return response.json(repositories[repositorieIndex]);
+
 });
 
 module.exports = app;
